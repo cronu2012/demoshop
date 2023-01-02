f356fcbfa158f337a4e33b21cfdee338a583fd5b
cronu <cronu2012@gmail.com>
2023-01-03 00:11:52 +0800

    新增測試MemberController類別

---
+++ src/test/java/com/steven/demoshop/controller/MemberControllerTest.java
@@ -0,0 +1,36 @@
+package com.steven.demoshop.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberControllerTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void modifyMember() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getMember() {
+    }
+
+    @Test
+    void getMembers() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void checkMemberParam() {
+    }
+}
